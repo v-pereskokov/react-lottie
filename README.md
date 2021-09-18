73a02bf8b2823c056772d4771911f5fbdb1df727
Vladislav Pereskokov <v.pereskokov@yandex-team.com>
2021-09-18 23:22:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+TODO
+
+1. lib
+2. lint
+3. jest
+4. storybook
---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+declare const Hello: React.FC<{}>;
+
+declare module 'hello' {
+}
+
+export default Hello;
